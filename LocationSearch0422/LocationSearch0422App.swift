1abfbfb8ac32ae7718c391de27a77b8d49dae4ea
张亚飞 <672233580@qq.com>
2022-04-22 12:36:20 +0800

    Initial Commit

---
+++ LocationSearch0422/LocationSearch0422App.swift
@@ -0,0 +1,17 @@
+//
+//  LocationSearch0422App.swift
+//  LocationSearch0422
+//
+//  Created by 张亚飞 on 2022/4/22.
+//
+
+import SwiftUI
+
+@main
+struct LocationSearch0422App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
