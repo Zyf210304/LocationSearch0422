40083973d4afa85346d7078ed1ce07933eece288
张亚飞 <672233580@qq.com>
2022-04-22 18:00:02 +0800

    地图可移动大头针

--- LocationSearch0422/ContentView.swift
+++ LocationSearch0422/ContentView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView {
+            
+            SearchView()
+                .navigationBarHidden(true)
+        }
     }
 }
 
